790ca9cc93201695e7648e4f1c30eb75e7d94f5d
lr154lrose <ioana_mara_c@yahoo.com>
2020-05-06 18:24:06 +0100

    added change to main function

--- haskell solutions/problem_11.hs
+++ haskell solutions/problem_11.hs
@@ -63,7 +63,6 @@ main = do
                     \20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54\n\
                     \01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"
 
-    print((map read (words (lines grid !! 0)) :: [Int]) !! 2)
-    print(find_horizontally (map read (words (lines grid !! 0)) :: [Int]))
-    print(diagonal_product2 grid)
+    let result = max (max (horizontal_product grid) (vertical_product grid)) (max (diagonal_product1 grid) (diagonal_product2 grid))
+    print(result)
  

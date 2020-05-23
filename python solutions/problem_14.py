5ec95c43c89ae4252b8f3e6a4da945bef7576332
lr154lrose <ioana_mara_c@yahoo.com>
2020-05-23 20:01:02 +0100

    added solution for problem 14 in Python

---
+++ python solutions/problem_14.py
@@ -0,0 +1,29 @@
+"""
+Program finding starting number (under one million) which
+produces the longest Collatz sequence
+"""
+
+def collatz(n, sequence_elements):
+    if  n == 1:
+        return sequence_elements + 1
+    elif n%2 == 0:
+        return collatz(n//2, sequence_elements + 1)
+    else:
+        return collatz(3*n + 1, sequence_elements + 1)
+
+
+def compute():
+    biggest_sequence = 0
+    number = 0
+    i = 1
+    while(i < 1000000):
+        current_iteration = collatz(i, 0)
+        if(current_iteration > biggest_sequence):
+            biggest_sequence = current_iteration
+            number = i
+        i += 1
+
+    return number
+
+if __name__=="__main__":
+    print(compute())

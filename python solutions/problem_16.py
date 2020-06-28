7a7d7ed79a6b16a6ff788a359974f04283c68c01
Ioana Mara <ioana_mara_c@yahoo.com>
2020-06-28 15:40:56 +0100

    solved problem 16

---
+++ python solutions/problem_16.py
@@ -0,0 +1,15 @@
+#Program finding the sum of the digits of a power of 2
+
+def power_of_two(power):
+    return pow(2, power)
+
+def sum_of_digits(number):
+    string_number = str(number)
+    sum_digits = 0
+    for digit in string_number:
+        sum_digits += int(digit)
+
+    return sum_digits
+
+if __name__ == "__main__":
+    print(sum_of_digits(power_of_two(1000)))
